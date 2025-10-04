c3ea2c42c29cf270d32d0ef79a58d0ce6ab703c5
Marcello <marcellosantos1255@gmail.com>
2025-10-04 12:04:35 -0300

    .

---
+++ formulario.js
@@ -0,0 +1,18 @@
+// const form = document.getElementById('meuForm')
+
+// form.addEventListener('submit',function (event){
+//     event.preventDefault()
+//     console.log(event)
+//     alert('enviado')
+// })
+
+// =====================================
+
+// function somaNotas (nota1, nota2){
+//     let soma= nota1+nota2
+//     console.log(soma)
+// }
+
+// somaNotas (5,9)
+// somaNotas (3,3)
+// somaNotas (4,4)
